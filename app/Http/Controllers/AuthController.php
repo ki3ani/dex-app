639b465446d43fa54163f243c14a73cbee2a53a5
Shadrack K Kimutai <shadrackkimutai@gmail.com>
2023-04-20 19:41:39 +0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,9 +103,9 @@ class AuthController extends Controller
             $productionlist=$productionlist->groupby('production_date');
             $productionlist->all();
             //$dates = $this->generateDates($start, $end); // you fill zero valued dates
-            $labels=$productionlist->keys();
-            $productionvalues=$productionlist->values();
-            //dd($productionvalues[0][0]);
+            $labels=json_encode($productionlist->keys());
+            $productionvalues=json_encode($productionlist->values());
+            //dd($labels,);
             return view('dashboard',compact('production','daysproduction','production_time','herd','users','labels','productionvalues'));
         }
   
